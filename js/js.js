9d1a89dd741196e014bd8e9da77e9d8cb11a3002
GUOGUO13 <1147971088@qq.com>
2018-03-29 18:05:50 +0800

    修复active状态

--- js/js.js
+++ js/js.js
@@ -22,6 +22,8 @@ class Slider {
             min: 0,
             max: 100,
             isClick: false,
+            isMinHandle: false,
+            isMaxHandle: false,
             callback: () => {}
         }
         this.el = { node };
@@ -33,14 +35,16 @@ class Slider {
 
     /* 
     * 初始化滑杆
-    * state 包括 最小值，最大值，滑杆盒子位置、宽度，比率，是否点击 
+    * state 包括 最小值，最大值，滑杆盒子位置、宽度，比率，是否点击,是否左手柄操作，是否右手柄操作 
+    * newValue 用于存储事件操作后最大值与最小值
     */
     setDefaultSlider () {
-        const { min, max, isClick } = this.option;
+        const { min, max, isClick, isMinHandle, isMaxHandle } = this.option;
         const { node } = this.el;
         const { left, width } = node.getBoundingClientRect();
         const ratio = (max - min) / width; 
-        this.state = { min, max, left, width, ratio, isClick};
+        this.newValue = { min, max };
+        this.state = { min, max, left, width, ratio, isClick, isMinHandle, isMaxHandle };
         let html = `
         <p class="slider-range-tip">
             <span class="min">${min}</span>
@@ -74,22 +78,20 @@ class Slider {
 
         // 鼠标移开
         Slider.on(window, onend, e => {
-            const { isClick } = this.state;
-            if(isClick) {
-                this.state.isClick = false;
-            }
-
+            this.state.isClick ? this.state.isClick = false: "";
+            this.state.isMinHandle ? this.state.isMinHandle = false: "";
+            this.state.isMaxHandle ? this.state.isMaxHandle = false: "";
+            Slider.getClass("slider-handle-min").classList.remove("active");
+            Slider.getClass("slider-handle-max").classList.remove("active");
         })
 
         // 鼠标移动
         Slider.on(window, onmove, e => {
-            const { isClick } = this.state;
-            if( isClick ) {
+            if( this.state.isClick ) {
                 const event = getEvent(e);
                 const clientX = event.clientX;
-                this.handleSlider(clientX); 
+                this.handleSlider(clientX);
             }
-            Slider.getClass("slider-handle").classList.remove("active");
         })
 
     }
@@ -100,37 +102,62 @@ class Slider {
         const maxLeft = Slider.getClass("slider-handle-max").offsetLeft;
         const { min, max, left, width, ratio } = this.state;
         const moveRange = clientX - left;
-        const value = Math.round( moveRange*ratio + min );
+        const value = Math.round( moveRange * ratio + min );
         this.state.moveRange = moveRange;
         this.state.minLeft = minLeft;
         this.state.maxLeft = maxLeft;
-        if( moveRange - minLeft >= (maxLeft - minLeft)/2) {
+        if(value > max || value < min ) {
+            return;
+        } 
+        // 这里存在的问题，如果当前符合这个条件，但是鼠标再次点击，在这里就卡住了
+        if(this.newValue.min + 1 >= this.newValue.max) {   
+            return;
+        };
+        if( moveRange >= maxLeft || ( moveRange > minLeft && maxLeft - moveRange < moveRange - minLeft ) ) {
+            this.state.isMaxHandle = true;
+            this.state.isMinHandle = false;
             this.state.maxLeft = moveRange;
-            this.handleDom("max", value);
+            this.newValue.max = value; 
         } else {
+            this.state.isMinHandle = true;
+            this.state.isMaxHandle = false;
             this.state.minLeft = moveRange;
-            this.handleDom("min", value);
+            this.newValue.min = value; 
         }
+        this.handleDom(value);
     }
 
-    handleDom (handles, value) {
+    handleDom (value) {
         const { min, max, width, moveRange, minLeft, maxLeft } = this.state;
-        const handle = Slider.getClass("slider-handle-" + handles);
-        const tip = Slider.getClass(handles);  
+        const maxHandle = Slider.getClass("slider-handle-max");
+        const minHandle = Slider.getClass("slider-handle-min");
+        const minTip = Slider.getClass("min");  
+        const maxTip = Slider.getClass("max");  
         const bar = Slider.getClass("slider-bar");
-        if(value > max || value < min) {
-            return;
+        const barWidth = maxLeft - minLeft;
+        let curHandle, curTip;
+        if(this.state.isMaxHandle) {
+            curHandle = maxHandle;
+            minHandle.classList.remove("active");
+            curTip = maxTip;
+        } else {
+            curHandle = minHandle;
+            maxHandle.classList.remove("active");
+            curTip = minTip;
         }
+
         //数值赋值 
-        tip.textContent = value;
+        curTip.textContent = value;
 
         //手柄移位
-        handle.classList.add("active");
-        handle.style.left = moveRange + 'px';
+        curHandle.classList.add("active");
+        curHandle.style.left = moveRange + 'px';
 
         //设置滑杆样式
-        bar.style.width = maxLeft - minLeft + 'px';
+        bar.style.width = barWidth + 'px';
         bar.style.left = minLeft + 'px';
+
+        this.option.callback(this.newValue.min, this.newValue.max);
     }
 }
 
